a33f3dc0861cdf801ba0f2ef4ec4f3f8304b5d70
橘猫昊昊 <pichuworks@live.com>
2018-09-10 09:50:07 +0800

    database update

--- app/src/main/java/com/ProjectViolet/emotionDiary/db/DiaryDatabaseHelper.java
+++ app/src/main/java/com/ProjectViolet/emotionDiary/db/DiaryDatabaseHelper.java
@@ -7,12 +7,34 @@ import android.database.sqlite.SQLiteOpenHelper;
 
 public class DiaryDatabaseHelper extends SQLiteOpenHelper {
 
+    public static final String CREATE_USER = "create table DiaryUser("
+            + "id integer primary key autoincrement, "
+            + "username text, "
+            + "password text, "
+            + "regTime text,"
+            + "privilege integer)";  // priv == 0 -> user, priv == 1 -> admin
+
     public static final String CREATE_DIARY = "create table Diary("
             + "id integer primary key autoincrement, "
             + "date text, "
             + "title text, "
             + "tag text, "
+            + "userid integer, "
+            + "analysedFlag integer, "
             + "content text)";
+
+    public static final String CREATE_TEXT_SENTIMENT = "create table DiaryTextSentiment("
+            + "id integer primary key autoincrement, "
+            + "userID integer, "
+            + "diaryID integer, "
+            + "resultArray text, "
+            + "updateTime text)";
+
+    public static final String CREATE_SENTIMENT_ANALYSIS = "create table DiarySentimentAnalysis("
+            + "id integer primary key autoincrement, "
+            + "userID integer, "
+            + "featureVector text, "
+            + "classifier text)";
 
     private Context mContext;
     public DiaryDatabaseHelper(Context context, String name, SQLiteDatabase.CursorFactory factory, int version){
@@ -21,12 +43,14 @@ public class DiaryDatabaseHelper extends SQLiteOpenHelper {
     }
     @Override
     public void onCreate(SQLiteDatabase db) {
+        db.execSQL(CREATE_USER);
         db.execSQL(CREATE_DIARY);
+        db.execSQL(CREATE_TEXT_SENTIMENT);
+        db.execSQL(CREATE_SENTIMENT_ANALYSIS);
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
-
         db.execSQL("drop table if exists Diary");
         onCreate(db);
     }
